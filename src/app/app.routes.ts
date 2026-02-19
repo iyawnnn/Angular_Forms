3d0d5b657fdd19ffe312b3f4cce5c54b66d5b15e
Ian Macabulos <iannmacabulos@gmail.com>
2026-02-19 09:04:15 +0800

    fix: add route to custom form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TemplateDemo } from './template-demo/template-demo';
 import { ReactiveDemo } from './reactive-demo/reactive-demo';
+import { CustomForm } from './custom-form/custom-form';
 
 export const routes: Routes = [
   { path: 'template-demo', component: TemplateDemo },
   { path: 'reactive-demo', component: ReactiveDemo },
+  { path: 'custom-form', component: CustomForm },
   { path: '', redirectTo: '/template-demo', pathMatch: 'full' }
 ];
